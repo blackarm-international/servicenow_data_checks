cd406038c63509c1bed94ac4c81869e423e0889f
blackarmVR <honungsbjorn@protonmail.com>
2022-03-26 12:52:12 +0000

    :)

--- bad_ci_relations.js
+++ bad_ci_relations.js
@@ -1,4 +1,4 @@
-var roomName = 'P3SJ';
+var roomName = 'N3SB';
 var roomSysId = '';
 // find the sys_id of the room
 var grRoom = new GlideRecord('cmdb_ci_computer_room');
@@ -27,29 +27,29 @@ while (grRack.next()) {
 }
 // find CIs parented to the rack
 var ciList = [];
-var grRel = new GlideRecord('cmdb_rel_ci');
-grRel.addQuery('type', 'e76b8c7b0a0a0aa70082c9f7c2f9dc64');
-grRel.addQuery('parent.sys_id', 'IN', rackCiList);
-grRel.query();
-while (grRel.next()) {
-  ciList.push(grRel.getValue('child'));
+var grMounted = new GlideRecord('cmdb_rel_ci');
+grMounted.addQuery('type', 'e76b8c7b0a0a0aa70082c9f7c2f9dc64');
+grMounted.addQuery('parent.sys_id', 'IN', rackCiList);
+grMounted.query();
+while (grMounted.next()) {
+  ciList.push(grMounted.getValue('child'));
 }
 // find CIs parented to the above CIs
-var grRel = new GlideRecord('cmdb_rel_ci');
-grRel.addQuery('type', '55c95bf6c0a8010e0118ec7056ebc54d');
-grRel.addQuery('parent.sys_id', 'IN', ciList);
-grRel.query();
-while (grRel.next()) {
-  ciList.push(grRel.getValue('child'));
+var grSled = new GlideRecord('cmdb_rel_ci');
+grSled.addQuery('type', '55c95bf6c0a8010e0118ec7056ebc54d');
+grSled.addQuery('parent.sys_id', 'IN', ciList);
+grSled.query();
+while (grSled.next()) {
+  ciList.push(grSled.getValue('child'));
 }
 // find the CIs that do not are not installed
 var susList = [];
-var grCi = new GlideRecord('cmdb_ci');
-grCi.addQuery('sys_id', 'IN', ciList);
-grCi.query();
-while (grCi.next()) {
-  if (grCi.getValue('install_status') !== '1') {
-    susList.push('https://godaddy.service-now.com/cmdb_ci.do?sys_id=' + grCi.getUniqueValue());
+var grCmdbCi = new GlideRecord('cmdb_ci');
+grCmdbCi.addQuery('sys_id', 'IN', ciList);
+grCmdbCi.query();
+while (grCmdbCi.next()) {
+  if (grCmdbCi.getValue('install_status') !== '1') {
+    susList.push('https://godaddy.service-now.com/cmdb_ci.do?sys_id=' + grCmdbCi.getUniqueValue());
   }
 }
 gs.print('Number of suspicious CIs found = ' + susList.length);
