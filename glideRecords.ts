1b9e777862959259d40816d986419a801686cbdb
blackarmVR <honungsbjorn@protonmail.com>
2022-04-02 12:27:56 +0100

    :)

--- glideRecords.ts
+++ glideRecords.ts
@@ -5,35 +5,43 @@ let exampleString: string | null;
 let exampleUnixTime: number | null;
 let test: any;
 //
-exampleBoolean = null;
-test = gr.getValue('xxxxx');
-if (test === '0') {
-  exampleBoolean = false;
-}
-if (test === '1') {
-  exampleBoolean = true;
-}
-//
-exampleFloat = null;
-test = gr.getValue('xxxxx');
-if (!isNaN(parseFloat(test))) {
-  exampleFloat = parseFloat(test);
-}
-//
-exampleInteger = null;
-test = gr.getValue('xxxxx');
-if (!isNaN(parseInt(test))) {
-  exampleInteger = parseInt(test);
-}
-//
-exampleString = null;
-test = gr.getValue('xxxxx');
-if (typeof test === 'string' && test !== '') {
-  exampleString = test;
-}
-//
-exampleUnixTime = null;
-test = gr.getValue('xxxxx');
-if (new GlideDateTime(test).getNumericValue() !== 0) {
-  exampleUnixTime = new GlideDateTime(test).getNumericValue();
+// @ts-ignore
+const gr = GlideRecord('xxxxxxxx');
+gr.query();
+while(gr.next()) {
+  //
+  exampleBoolean = null;
+  test = gr.getValue('xxxxx');
+  if (test === '0') {
+    exampleBoolean = false;
+  }
+  if (test === '1') {
+    exampleBoolean = true;
+  }
+  //
+  exampleFloat = null;
+  test = gr.getValue('xxxxx');
+  if (!isNaN(parseFloat(test))) {
+    exampleFloat = parseFloat(test);
+  }
+  //
+  exampleInteger = null;
+  test = gr.getValue('xxxxx');
+  if (!isNaN(parseInt(test))) {
+    exampleInteger = parseInt(test);
+  }
+  //
+  exampleString = null;
+  test = gr.getValue('xxxxx');
+  if (typeof test === 'string' && test !== '') {
+    exampleString = test;
+  }
+  //
+  exampleUnixTime = null;
+  test = gr.getValue('xxxxx');
+  // @ts-ignore
+  if (new GlideDateTime(test).getNumericValue() !== 0) {
+    // @ts-ignore
+    exampleUnixTime = new GlideDateTime(test).getNumericValue();
+  }
 }
