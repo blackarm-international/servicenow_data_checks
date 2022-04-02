f775957ed609b3661e7ca0bc72fc0869c9027254
blackarmVR <honungsbjorn@protonmail.com>
2022-04-02 12:06:45 +0100

    strict data checking

---
+++ glideRecords.js
@@ -0,0 +1,32 @@
+let exampleBoolean: boolean | null;
+let exampleFloat: number | null;
+let exampleInteger: number | null;
+let exampleString: string | null;
+let test: any;
+//
+exampleBoolean = null;
+test = gr.getValue('xxxxx');
+if (test === '0') {
+  exampleBoolean = false;
+}
+if (test === '1') {
+  exampleBoolean = true;
+}
+//
+exampleFloat = null;
+test = gr.getValue('xxxxx');
+if (!isNaN(parseFloat(test))) {
+  exampleFloat = parseFloat(test);
+}
+//
+exampleInteger = null;
+test = gr.getValue('xxxxx');
+if (!isNaN(parseInt(test))) {
+  exampleInteger = parseInt(test);
+}
+//
+exampleString = null;
+test = gr.getValue('xxxxx');
+if (typeof test === 'string' && test !== '') {
+  exampleString = test;
+}
